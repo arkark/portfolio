8df076a3d26b42e249d436f701063a857d9d2357
Ark <target.ark@gmail.com>
2019-03-16 01:01:34 +0900

    Update .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,19 +11,6 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "warn"
   },
-  // rules: {
-  //   "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
-  //   "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
-  //   "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "warn",
-  //   "no-empty": process.env.NODE_ENV === "production" ? "error" : "warn",
-  //   "no-unreachable": process.env.NODE_ENV === "production" ? "error" : "warn",
-  //   semi: ["error", "always"],
-  //   "semi-style": ["error", "last"],
-  //   "no-extra-semi": "error",
-  //   "space-before-function-paren": ["error", "always"],
-  //   // vue
-  //   "vue/no-unused-components": process.env.NODE_ENV === "production" ? "error" : "warn",
-  // },
   parserOptions: {
     parser: "babel-eslint"
   }
