93db0696237b06d9f201f86fa3d4e9d041b3831f
Takeshi Kaneko <target.ark@gmail.com>
2019-07-04 05:00:57 +0900

    Fix a broken link

--- src/data/workList.js
+++ src/data/workList.js
@@ -13,7 +13,7 @@ export default [
     title: "latexmk-docker",
     url: "https://github.com/ArkArk/latexmk-docker",
     src:
-      "https://github.com/ArkArk/latexmk-docker/blob/master/img/demo.png?raw=true",
+      "https://github.com/ArkArk/latexmk-docker/blob/master/screenshot.png?raw=true",
     group: "Tool",
     description: "Docker上で動く快適なLatex環境",
     date: "2019/1"
