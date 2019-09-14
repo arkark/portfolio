febb32fab21be0b55963dc6e8080b31e9c68cd34
Takeshi Kaneko <target.ark@gmail.com>
2019-09-14 19:50:19 +0900

    Update intern list

--- src/data/intern.js
+++ src/data/intern.js
@@ -8,7 +8,26 @@ export default {
       label: "2019",
       children: [
         {
-          id: "1-201901",
+          id: "1-2019-5",
+          label:
+            "ウォンテッドリー株式会社：Wantedly Summer Intern, Infrastructure"
+        },
+        {
+          id: "1-2019-4",
+          label: "株式会社ミクシィ：2Days Challenge"
+        },
+        {
+          id: "1-2019-3",
+          label:
+            "クックパッド株式会社：Cookpad Summer Internship 2019 10 Day Tech",
+          url: "https://ark4rk.hatenablog.com/entry/2019/09/03/202429"
+        },
+        {
+          id: "1-2019-2",
+          label: "グリー株式会社：GREE Camp"
+        },
+        {
+          id: "1-2019-1",
           label:
             "クックパッド株式会社：Cookpad Spring 1day Internship 2019 コンテナコース",
           url: "https://ark4rk.hatenablog.com/entry/2019/03/20/220442"
@@ -30,7 +49,7 @@ export default {
         },
         {
           id: "1-2017-2",
-          label: "株式会社ミクシィ：第6回 git challenge（第2位）",
+          label: "株式会社ミクシィ：第6回 git challenge",
           url: "https://ark4rk.hatenablog.com/entry/2017/09/03/173836"
         },
         {
