ebfbf2473600388ccd8816456ab28fe4e6965cf8
Takeshi Kaneko <target.ark@gmail.com>
2020-03-09 18:54:20 +0900

    Use vuetify-loader

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,12 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
-import "vuetify/dist/vuetify.min.css";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
+const opts = {
   icons: {
-    iconfont: "md"
+    iconfont: "fa"
   }
-});
+};
+
+export default new Vuetify(opts);
