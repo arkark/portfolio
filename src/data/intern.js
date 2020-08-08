2fa38fe6b41e7c0fb88a1d51af5c1524dd9e8a31
Takeshi Kaneko <target.ark@gmail.com>
2020-08-09 05:32:01 +0900

    Update intern.js

--- src/data/intern.js
+++ src/data/intern.js
@@ -3,28 +3,26 @@ export default [
     label: "2019",
     children: [
       {
-        label: "株式会社VOYAGE GROUP : Sunrise 大規模サービス構築プログラム",
+        label:
+          "株式会社VOYAGE GROUP : Sunrise 大規模サービス構築プログラム (3 days)",
       },
       {
         label:
-          "ウォンテッドリー株式会社 : Wantedly Summer Intern, Infrastructure",
-        blog: "https://ark4rk.hatenablog.com/entry/2019/10/21/155106",
+          "ウォンテッドリー株式会社 : Wantedly Summer Intern, Infrastructure (3 weeks)",
       },
       {
-        label: "株式会社ミクシィ : 2Days Challenge",
-      },
-      {
-        label:
-          "クックパッド株式会社 : Cookpad Summer Internship 2019 10 Day Tech",
-        blog: "https://ark4rk.hatenablog.com/entry/2019/09/03/202429",
-      },
-      {
-        label: "グリー株式会社 : GREE Camp",
+        label: "株式会社ミクシィ : 2Days Challenge (2 days)",
       },
       {
         label:
-          "クックパッド株式会社 : Cookpad Spring 1day Internship 2019 コンテナコース",
-        blog: "https://ark4rk.hatenablog.com/entry/2019/03/20/220442",
+          "クックパッド株式会社 : Cookpad Summer Internship 2019 10 Day Tech (2 weeks)",
+      },
+      {
+        label: "グリー株式会社 : GREE Camp (2 days)",
+      },
+      {
+        label:
+          "クックパッド株式会社 : Cookpad Spring 1day Internship 2019 コンテナコース (1 day)",
       },
     ],
   },
@@ -32,19 +30,18 @@ export default [
     label: "2017",
     children: [
       {
-        label: "株式会社ハル研究所 : INTERNSHIP 2017 PROGRAMMER COURSE",
+        label: "株式会社ハル研究所 : INTERNSHIP 2017 PROGRAMMER COURSE (1 day)",
       },
       {
         label:
-          "株式会社コロプラ : MASH UP エンターテインメントを追求する合宿型インターンシップ in 箱根",
+          "株式会社コロプラ : MASH UP エンターテインメントを追求する合宿型インターンシップ (3 days)",
       },
       {
-        label: "株式会社ミクシィ : 第6回 git challenge",
-        blog: "https://ark4rk.hatenablog.com/entry/2017/09/03/173836",
+        label: "株式会社ミクシィ : 第6回 git challenge (1 day)",
       },
       {
         label:
-          "株式会社ディー・エヌ・エー : 2017 SUMMER INTERNSHIP ゲームエンジニアリングコース",
+          "株式会社ディー・エヌ・エー : 2017 SUMMER INTERNSHIP ゲームエンジニアリングコース (3 days)",
       },
     ],
   },
