4f844a9068b1ff75e07cd1de6b38b025ff727ae1
Takeshi Kaneko <target.ark@gmail.com>
2020-09-14 15:37:56 +0900

    Update CTF

--- src/data/ctf.js
+++ src/data/ctf.js
@@ -1,11 +1,16 @@
 export default [
   {
-    label: "TSG CTF 2020 : ./Vespiary 7位",
+    label: "InterKosenCTF 2020 : ./Vespiary 3位/133",
+    blog: "https://ark4rk.hatenablog.com/entry/2020/09/07/004439",
+    url: "https://theoremoon.github.io/InterKosenCTF2020-challenges/",
+  },
+  {
+    label: "TSG CTF 2020 : ./Vespiary 7位/627",
     blog: "https://ark4rk.hatenablog.com/entry/2020/07/13/164939",
     url: "https://ctftime.org/event/1004",
   },
   {
-    label: "SECCON 2019 CTF : ./Vespiary 国内決勝進出",
+    label: "SECCON 2019 CTF : ./Vespiary 37位/799 国内決勝進出",
     url: "https://www.seccon.jp/2019/seccon2019/",
   },
 ];
