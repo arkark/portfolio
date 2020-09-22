01d874188c898d2d94c0fc3177f837ac5ea94582
Takeshi Kaneko <target.ark@gmail.com>
2020-09-22 18:07:57 +0900

    Smallify subtitle

--- src/components/work/Work.vue
+++ src/components/work/Work.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-card-title primary-title class="px-3 pt-2 pb-0">
-      <p class="display-1 mb-0 pl-4 font-weight-medium">Works</p>
+      <h2 class="mb-0 pl-4 font-weight-medium">Works</h2>
     </v-card-title>
     <v-card-text class="pt-1 pb-2">
       <v-divider></v-divider>
