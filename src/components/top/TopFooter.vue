b6ac0cda906811272aa5adb42591133659fb5407
Takeshi Kaneko <target.ark@gmail.com>
2021-01-06 11:47:27 +0900

    Update year

--- src/components/top/TopFooter.vue
+++ src/components/top/TopFooter.vue
@@ -2,7 +2,7 @@
   <v-footer absolute color="#fff0" class="py-2 body-2" dark>
     <div class="flex-grow-1"></div>
     <v-icon dark x-small class="mr-1">far fa-copyright</v-icon>
-    <span>2015-2020 Ark</span>
+    <span>2015-2021 Ark</span>
     <v-btn
       href="https://github.com/arkark/portfolio"
       target="_blank"
