905344c109e90c250f27757805a1a6df6b7f3af5
Takeshi Kaneko <target.ark@gmail.com>
2026-05-07 23:08:27 +0900

    Fix Table component

--- src/components/utils/Table.tsx
+++ src/components/utils/Table.tsx
@@ -5,32 +5,32 @@ export type TableProps = TableType;
 const Table: React.FC<TableProps> = ({ columns, items }) => {
   return (
     <>
-      {/* <div className={`mt-4 overflow-auto`}> */}
-      <table className="table my-0 text-nowrap">
-        {columns && (
-          <thead>
-            <tr>
-              {columns.map((col, index) => (
-                <th key={index}>{col}</th>
-              ))}
-            </tr>
-          </thead>
-        )}
-        <tbody>
-          {items.map((rows, rowIndex) => (
-            <tr key={rowIndex}>
-              {rows.map((item, colIndex) =>
-                colIndex === 0 ? (
-                  <th key={`${rowIndex}-${colIndex}`}>{item}</th>
-                ) : (
-                  <td key={`${rowIndex}-${colIndex}`}>{item}</td>
-                ),
-              )}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      {/* </div> */}
+      <div className="overflow-x-auto">
+        <table className="table my-0 text-nowrap">
+          {columns && (
+            <thead>
+              <tr>
+                {columns.map((col, index) => (
+                  <th key={index}>{col}</th>
+                ))}
+              </tr>
+            </thead>
+          )}
+          <tbody>
+            {items.map((rows, rowIndex) => (
+              <tr key={rowIndex}>
+                {rows.map((item, colIndex) =>
+                  colIndex === 0 ? (
+                    <th key={`${rowIndex}-${colIndex}`}>{item}</th>
+                  ) : (
+                    <td key={`${rowIndex}-${colIndex}`}>{item}</td>
+                  ),
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
